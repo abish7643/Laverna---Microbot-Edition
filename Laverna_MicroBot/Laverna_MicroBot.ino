fc8c3b162382d4e8e0e834320fd8c8101ac1016f
abish7643 <abishvijayan7643@gmail.com>
2019-04-26 21:44:40 +0530

    Add files via upload

---
+++ Laverna_MicroBot/Laverna_MicroBot.ino
@@ -0,0 +1,73 @@
+int S_A = 11;  //speed motor a
+int M_A1 = 10; //motor a = +
+int M_A2 = 9; //motor a = -
+int M_B1 = 8; //motor b = -
+int M_B2 = 7; //motor b = +
+int S_B = 6;  //speed motor b
+
+int R_S = 2; //sincer R
+int S_S = 3; //sincer S
+int L_S = 4; //sincer L
+
+void setup() 
+{
+pinMode(M_B1, OUTPUT);
+pinMode(M_B2, OUTPUT);
+pinMode(M_A1, OUTPUT);
+pinMode(M_A2, OUTPUT);
+pinMode(S_B, OUTPUT);
+pinMode(S_A, OUTPUT);
+
+pinMode(L_S, INPUT);
+pinMode(S_S, INPUT);
+pinMode(R_S, INPUT);
+
+analogWrite(S_A, 200); 
+analogWrite(S_B, 200); 
+delay(200);
+}
+void loop()
+{  
+if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 0)){forword();}
+
+if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 0)){turnLeft();}
+if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) ==0)&&(digitalRead(R_S) == 0)) {turnLeft();}
+
+if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 1)){turnRight();}
+if ((digitalRead(L_S) == 0)&&(digitalRead(S_S) == 0)&&(digitalRead(R_S) == 1)){turnRight();}
+
+//if ((digitalRead(L_S) == 1)&&(digitalRead(S_S) == 1)&&(digitalRead(R_S) == 1)){Stop();}
+}
+
+void forword(){
+digitalWrite(M_A1, LOW);
+digitalWrite(M_A2, HIGH);
+digitalWrite(M_B1, HIGH);
+digitalWrite(M_B2, LOW);  
+
+}
+
+
+void turnRight(){
+digitalWrite(M_A1, LOW);
+digitalWrite(M_A2, LOW);
+digitalWrite(M_B1, HIGH);
+digitalWrite(M_B2, LOW);  
+delay(30);
+}
+
+void turnLeft(){
+digitalWrite(M_A1, LOW);
+digitalWrite(M_A2, HIGH);
+digitalWrite(M_B1, LOW);
+digitalWrite(M_B2, LOW);
+delay(35);
+}
+/*
+void Stop(){
+digitalWrite(M_A1, LOW);
+digitalWrite(M_A2, LOW);
+digitalWrite(M_B1, LOW);
+digitalWrite(M_B2, LOW);
+}
+*/
